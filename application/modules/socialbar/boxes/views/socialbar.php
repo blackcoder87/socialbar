cc185f566f9f0e8e8ef4615137dbd99d05ac9c91
Slipi85 <52356917+Slipi85@users.noreply.github.com>
2019-11-25 21:03:24 +0100

    update

--- application/modules/socialbar/boxes/views/socialbar.php
+++ application/modules/socialbar/boxes/views/socialbar.php
@@ -1,10 +1,11 @@
+<link href="<?=$this->getBoxUrl('socialbar/static/css/socialbar.css') ?>" rel="stylesheet">
 <?php
 $socials = $this->get('socials');
 $socialMapper = $this->get('socialMapper');
 ?>
 
 <?php if (!empty($socials)): ?>
-    <ul>
+    <ul class="list-unstyled ilch_menu_ul">
         <?php foreach ($socials as $social): ?>
             <li class="list-group-item"><a href="<?=$this->escape($social->getLink()) ?>"><b><?=$this->escape($social->getText()) ?> ?></b></a></li>
         <?php endforeach; ?>
