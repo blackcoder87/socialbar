fcce90eb9f29b30251ff94205e89c9186f8c714c
Slipi85 <52356917+Slipi85@users.noreply.github.com>
2019-12-01 20:01:04 +0100

    Update config.php

--- application/modules/socialbar/config/config.php
+++ application/modules/socialbar/config/config.php
@@ -51,12 +51,12 @@ class Config extends \Ilch\Config\Install
     public function getInstallSql()
     {
 			return	'CREATE TABLE IF NOT EXISTS `[prefix]_socialbar` (
-      					`id` INT(11) NOT NULL AUTO_INCREMENT,
-      					`icon` MEDIUMTEXT NOT NULL,
-      					`link` VARCHAR(255) NOT NULL,
-      					`text` MEDIUMTEXT NOT NULL,
-      					PRIMARY KEY (`id`)
-      					) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1';
+      				`id` INT(11) NOT NULL AUTO_INCREMENT,
+      				`icon` MEDIUMTEXT NOT NULL,
+      				`link` VARCHAR(255) NOT NULL,
+      				`text` MEDIUMTEXT NOT NULL,
+      				PRIMARY KEY (`id`)
+      				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1';
     }
 
     public function getUpdate($installedVersion)
