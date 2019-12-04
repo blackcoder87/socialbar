093d2c95d6fbd1b310da13d3c54d00e792a3a47f
Slipi85 <52356917+Slipi85@users.noreply.github.com>
2019-12-04 19:50:04 +0100

    Create Socialbar.php

---
+++ application/modules/socialbar/boxes/Socialbar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Socialbar\Boxes;
+
+use Modules\Socialbar\Mappers\Socialbar as SocialMapper;
+
+class Socialbar extends \Ilch\Box
+{
+    public function render()
+    {
+        $socialMapper = new SocialMapper();
+
+        $socials = $socialMapper->getSocial();
+        $this->getView()->set('socials', $socials);
+    }
+}
